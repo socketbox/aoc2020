541f8853d3ffd44a24a78714fa282354cc85f4a8
Casey Boettcher <chb@socketbox.com>
2020-12-12 23:10:10 -0500

    Close. Added leading zero and trailing 'device'

--- day10/day10b.go
+++ day10/day10b.go
@@ -19,8 +19,8 @@ func main() {
 	reader := bufio.NewReader(file)
 	var rsErr error	
 	var str string 
-	var intArr [11]int
-	for i:=0; i < len(intArr); i++ {
+	var intArr [105]int
+	for i:=1; i < len(intArr); i++ {
 		str, rsErr = reader.ReadString(0x0a)
 		if rsErr != nil && rsErr == io.EOF {
 			fmt.Println(rsErr)
@@ -30,14 +30,18 @@ func main() {
 		}
 	}
 	sort.Ints(intArr[:])
+	intArr[0]=0
+	intArr[104]=intArr[103]+3
 	var cmbCt, prod int	
 	for i := 0; i < len(intArr); i++ {
+		fmt.Println("prod:", prod, "cmbCt", cmbCt)	
 		if i == 0 {
 			prod = 1
 			cmbCt = -1
-		} else if intArr[i] - 3 == intArr[i-1] {
-		//is the current int three greater than its predecessor?
-		prod *= (cmbCt * 2)	//can get away with this b/c base is never more than 2
+		} else if intArr[i] - 3 == intArr[i-1] { //is the current int three greater than its predecessor?
+			if cmbCt > 0 {
+				prod *= (cmbCt * 2)	//can get away with this b/c base is never more than 2
+			}
 			cmbCt = -1
 		} else if intArr[i] - 1 == intArr[i-1] {
 			cmbCt++
